00eba9b373fc0b49591cabfe4c4f9f4a74d146a4
HsinCheng Chao <olala7846@gmail.com>
2017-10-15 12:43:40 +0800

    Add ros/docker destkop support

---
+++ run_docker+_desktop.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+IP=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
+echo "adding IP $IP to xhost"
+/opt/X11/bin/xhost + $IP
+
+docker run --rm -it \
+  --name capstone \
+  -e DISPLAY=$IP:0 \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -v $PWD:/capstone/ \
+  -v /tmp/log:/root/.ros/ \
+  capstone
